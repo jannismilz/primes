826055e3533a146c858df99ebbacecbaa5a7df01
Jannis Milz <jannis.milz@bsi-software.com>
2025-06-16 14:12:38 +0200

    Start with mersenne script

---
+++ huge_mersenne/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+//
+// 2^p - 1
+//
+
+// GOAL: 333e6 < p < 1 billion (around 35 million primes)
+// GOAL: On average 1 second per p
+
+// Create a log file with all possible n's and their current processing step
+
+//
+// Finding easy factors
+//
+
+// Trial factoring
+// Each factor q is of form 2kp + 1 and q must be 1 or 7 mod 8
+// Modified sieve of eratosthenes
+// Trial factoring with the powering algorithm from GIMPS
+
+// P-1 method
+
+//
+// Finding a probably prime
+//
+// PRP
+
+//
+// Last needed test if probable prime
+//
+// Lucas-Lehmer test
+
+func main() {
+	fmt.Println("Hello, World!")
+}
